49a053114fb415d3313660ba012497788d952808
FellSoma <n.felix2003@gmail.com>
2022-02-03 19:33:48 +0500

    Добавьте файлы проекта.

---
+++ Fitnes/Home.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace Fitnes
+{
+    /// <summary>
+    /// Логика взаимодействия для Home.xaml
+    /// </summary>
+    public partial class Home : Window
+    {
+       
+        public Home()
+        {
+            InitializeComponent();
+        
+        }
+       
+    }
+}
---
+++ Fitnes/SiningMenu.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Fitnes
+{
+    /// <summary>
+    /// Логика взаимодействия для SiningMenu.xaml
+    /// </summary>
+    public partial class SiningMenu : Window
+    {
+        public SiningMenu()
+        {
+            InitializeComponent();
+            
+           
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            Window g = new SiningMenu();
+            g.Show();
+        }
+
+        private void close(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private void PasswordBox_IsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+
+        private void onPasswordChenged(object sender, RoutedEventArgs e)
+        {
+            if (passwordBx.Password.Length > 0)
+            {
+                WaterMark.Visibility = Visibility.Collapsed;
+                //passwordBx.IsSelectionActive
+            }
+            else
+            {
+                WaterMark.Visibility = Visibility.Visible;
+
+            }
+        }
+
+        private void emptyMethod(object sender, DependencyPropertyChangedEventArgs e)
+        {
+        }
+
+        private void entrance(object sender, RoutedEventArgs e)
+        {
+            Window g = new SiningMenu();
+            g.Show();
+        }
+
+        private void windowState(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void ToolBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void Loading(object sender, RoutedEventArgs e)
+        {
+            Window g = new RegMenu();
+            g.Show();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Window_Activated(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
