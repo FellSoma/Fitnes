aee01932565272da643dc9375d46f1f9eeb82442
FellSoma <n.felix2003@gmail.com>
2022-02-03 19:19:51 +0500

    Add project.

---
+++ Fitnes/Fitnes/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Fitnes
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer.Interval = TimeSpan.FromMilliseconds(100);
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+        int sec = 0;
+
+
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            sec++;
+            if (sec >= 1)
+            {
+                if (sec >= 2)
+                {
+                    if (sec >= 3)
+                    {
+                        FirstLogo.Visibility = Visibility.Visible;
+                        SecondLogo.Visibility = Visibility.Collapsed;
+                        FinishLogo.Visibility = Visibility.Collapsed;
+                        if (sec >= 4)
+                        {
+                            sec = 0;
+                            timer.Stop();
+                            Window g = new SiningMenu();
+                            g.Show();
+                            this.Close();
+                        }
+                        return;
+                    }
+                    FirstLogo.Visibility = Visibility.Collapsed;
+                    SecondLogo.Visibility = Visibility.Collapsed;
+                    FinishLogo.Visibility = Visibility.Visible;
+                    return;
+                }
+                FirstLogo.Visibility = Visibility.Collapsed;
+                SecondLogo.Visibility = Visibility.Visible;
+                FinishLogo.Visibility = Visibility.Collapsed;
+                return;
+            }
+
+        }
+    }
+}
---
+++ Fitnes/Fitnes/RegMenu.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Fitnes
+{
+    /// <summary>
+    /// Логика взаимодействия для RegMenu.xaml
+    /// </summary>
+    public partial class RegMenu : Window
+    {
+        public RegMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            Window g = new SiningMenu();
+            g.Show();
+        }
+
+        private void close(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private void PasswordBox_IsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+           
+        }
+
+        private void onPasswordChenged(object sender, RoutedEventArgs e)
+        {
+            if (passwordBx.Password.Length > 0)
+            {
+                WaterMark.Visibility = Visibility.Collapsed;
+                //passwordBx.IsSelectionActive
+            }
+            else
+            {
+                WaterMark.Visibility = Visibility.Visible;
+
+            }
+
+            if (passwordBx2.Password.Length > 0)
+            {
+                WaterMark1.Visibility = Visibility.Collapsed;
+                //passwordBx.IsSelectionActive
+            }
+            else
+            {
+                WaterMark1.Visibility = Visibility.Visible;
+
+            }
+        }
+
+        private void emptyMethod(object sender, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+
+        private void entrance(object sender, RoutedEventArgs e)
+        {
+            Window g = new SiningMenu();
+            g.Show();
+        }
+
+        private void windowState(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void ToolBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void Loading(object sender, RoutedEventArgs e)
+        {
+            Window g = new RegMenu();
+            g.Show();
+        }
+    }
+}
